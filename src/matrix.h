8c298f6974c992de6ddb81e66e254e10c7f31378
VoxelPi <verblockt@gmail.com>
2023-10-05 13:51:39 +0200

    Add matrix class

---
+++ src/matrix.h
@@ -0,0 +1,13 @@
+#ifndef FILE_MATRIX_H
+#define FILE_MATRIX_H
+
+namespace ASC_bla
+{
+    enum ORDERING { ColMajor, RowMajor };
+    template <typename T, ORDERING ORD>
+    class Matrix {
+    
+    };
+}
+
+#endif
